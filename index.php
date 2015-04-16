0fb563d1b44a87c748cf18fe38fcfdfbc570e57a
Justine Cocchi <jcocchi@usc.edu>
2015-04-15 19:59:31 -0700

    consolidated backend files for clarity

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $app->get('/keyword/:keyword', function($keyword) use ($app){
 	$arxiv = new arxiv();
 	$items = $arxiv->queryByKeyword($keyword);
 
-	$app->render('getKeyword.php', ['items' => $items]);
+	$app->render('display.php', ['data' => $items]);
 });
 
 //implements the search by name functionality
@@ -28,7 +28,7 @@ $app->get('/name/:name', function($name) use ($app){
 	$arxiv = new arxiv();
 	$names = $arxiv->queryByName($name);
 
-	$app->render('getResearcher.php', ['names' => $names]);
+	$app->render('display.php', ['data' => $names]);
 });
 
 //implements the autocomplete functionality
@@ -48,7 +48,7 @@ $app->get('/IEEE/keyword/:keyword', function($keyword) use ($app){
 	$IEEE = new IEEE();
 	$items = $IEEE->queryByKeyword($keyword);
 
-	$app->render('getKeyword.php', ['items' => $items]);
+	$app->render('display.php', ['data' => $items]);
 });
 
 $app->get('/IEEE/name/:name', function($name) use ($app){
@@ -56,7 +56,7 @@ $app->get('/IEEE/name/:name', function($name) use ($app){
 	$IEEE = new IEEE();
 	$names = $IEEE->queryByName($name);
 
-	$app->render('getResearcher.php', ['names' => $names]);
+	$app->render('display.php', ['data' => $names]);
 });
 
 $app->get('/IEEE/id/:id', function($id) use ($app){
@@ -64,7 +64,7 @@ $app->get('/IEEE/id/:id', function($id) use ($app){
 	$IEEE = new IEEE();
 	$items = $IEEE->queryByID($id);
 
-	$app->render('getKeyword.php', ['items' => $items]);
+	$app->render('display.php', ['data' => $items]);
 });
 
 //v2
@@ -73,7 +73,7 @@ $app->get('/v2/keyword/:keyword', function($keyword) use ($app){
 	$arxiv = new arxiv();
 	$items = $arxiv->queryByKeywordV2($keyword);
 
-	$app->render('getKeyword.php', ['items' => $items]);
+	$app->render('display.php', ['data' => $items]);
 });
 
 $app->get('/v2/name/:name', function($name) use ($app){
@@ -81,7 +81,7 @@ $app->get('/v2/name/:name', function($name) use ($app){
 	$arxiv = new arxiv();
 	$names = $arxiv->queryByNameV2($name);
 
-	$app->render('getResearcher.php', ['names' => $names]);
+	$app->render('display.php', ['data' => $names]);
 });
 
 $app->get('/v2/title/:title', function($title) use ($app){
@@ -89,7 +89,7 @@ $app->get('/v2/title/:title', function($title) use ($app){
 	$arxiv = new arxiv();
 	$names = $arxiv->queryByTitle($title);
 
-	$app->render('getResearcher.php', ['names' => $names]);
+	$app->render('display.php', ['data' => $names]);
 });
 
 $app->run();
